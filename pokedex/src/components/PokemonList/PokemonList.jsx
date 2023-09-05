b1309df11cf2cd66ffad88ece29d16feed82e435
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-05 15:50:13 +0530

    Update PokemonList.jsx

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -2,18 +2,30 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 import './PokemonList.css';
 function PokemonList(){
-    const  [x ,setX] = useState(0);
-    const [y , setY] = useState(0);
+    // const  [x ,setX] = useState(0);
+    // const [y , setY] = useState(0);
+
+    const [pokemonList ,setpokemonList] = useState([]);
+    const [isLoading ,setIsLoading] = useState(true);
 
 
    async function downloadpokemons() {
         const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
-        console.log(response);
+        const pokemonResults =  response.data.results;
+        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+        const pokemonData = await axios.all(pokemonResultPromise)
+        console.log(pokemonData.data);
+         setIsLoading(false);
+
+       setPokemonList(pokemonData.map(pokemonData)=>{
+         const pokemon = pokeData.data;
+
+       })
     };
 
     useEffect(() =>{
         downloadpokemons();
-    }, [])
+    }, )
 
 
 
@@ -21,6 +33,7 @@ function PokemonList(){
         <>
        <div className="pokemon-list-wrapper">
          pokemon list
+         {(isLoading) ?  'Loading ....' : 'Data downloaded'}
        </div>
         
         
