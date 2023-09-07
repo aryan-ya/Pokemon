63561fbc92c97c810973e5147679f7ea0914ac00
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-09-07 12:47:19 +0530

    react-coursee

--- pokedex/src/components/PokemonDetails/PokemonDetails.jsx
+++ pokedex/src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,8 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function PokemonDetails() {
-    return(
-        <h1>hello</h1>
-    )
+  const { id } = useParams();
+  const [pokemon , setPokemon] = useState({});
+  console.log(id);
+
+  async function downloadPokemon(){
+   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+   setPokemon({
+   name: response.data.name,
+   image: response.data.sprites.other.dream_world.front_default,
+   weight : response.data.weight,
+   height : response.data.height,
+   types : response.data.types.map((t) => t.type.name)
+
+  })
+
+ 
 }
+  useEffect(() =>{
+    downloadPokemon();
+  },[]);
 
 
+  return(
+    <div className="pokemon-details-wrapper">
+        <div className="pokemon-name">name : {pokemon.name}</div>
+        <img className="pokemon-image"  src={pokemon.image}  />
+         <div>Height: {pokemon.height}</div>
+        <div>Weight: {pokemon.weight}</div>
+
+
+        <div className="pokemon-details-types">
+            {pokemon.types && pokemon.types.map((t) =>
+              <div key={t}>{t}</div>
+            )}
+        </div>
+    </div>
+  )
+
+
+
+  }
 export default PokemonDetails;
